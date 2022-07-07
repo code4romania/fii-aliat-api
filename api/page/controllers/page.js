78b0cfbd3e684a439db23abec2ef79b31cc951a7
Ciprian Platica <platica.ciprian@gmail.com>
2022-07-08 02:39:45 +0300

    Format home with counter data

--- api/page/controllers/page.js
+++ api/page/controllers/page.js
@@ -1,8 +1,32 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    let count;
+    if (ctx.query._q) {
+      entities = await strapi.services.page.search(ctx.query);
+    } else {
+      entities = await strapi.services.page.find(ctx.query);
+    }
+    const isHome = entities.map(({slug})=>slug).includes('home');
+    if (isHome) {
+      const counter = await strapi.services.counter.find();
+      count = counter.count;
+    }
+
+    return entities.map((entity) => {
+      if (entity.slug === 'home') {
+        return sanitizeEntity({...entity, components: entity.components.map((component)=> ({...component, title: `${count} ${component.title}`}))}, { model: strapi.models.page });
+      } else {
+        return sanitizeEntity(entity, { model: strapi.models.page });
+      }
+    });
+  },
+};
