58aed45bb9f6ae4b438c22f54ee1635afcd6a890
Ciprian Platica <platica.ciprian@gmail.com>
2022-07-08 01:51:31 +0300

    Add new stories as draft

--- api/story/services/story.js
+++ api/story/services/story.js
@@ -1,68 +1,32 @@
 'use strict';
 const { isDraft } = require('strapi-utils').contentTypes;
 const { omit, set } = require('lodash');
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
-//
-// module.exports = {
-//   async create(data, { files } = {}) {
-//     const storyModel =
-//       !data.hasLastNamePrivate && !data.victimLastName ?
-//         set(strapi.models.story, 'attributes.victimLastName.required', true) :
-//         strapi.models.story;
-//
-//     const validData =
-//       await strapi.entityValidator.validateEntityCreation(
-//         storyModel,
-//         data,
-//         { isDraft: isDraft(data, storyModel) }
-//       );
-//
-//     const entry = await strapi
-//       .query('story')
-//       .create({ ...validData, published_at: null });
-//
-//     if (files) {
-//       await strapi.entityService.uploadFiles(entry, files, {
-//         model: 'story',
-//       });
-//       return this.findOne({ id: entry.id });
-//     }
-//
-//     return entry;
-//   },
-//
-//   async find(params, populate) {
-//     const results = await strapi
-//       .query('story')
-//       .find(params, populate);
-//
-//     return results.filter((story) => {
-//       if (!story.published_at) {
-//         return false;
-//       }
-//
-//       return true;
-//     }).map((story) => {
-//       if (story.hasLastNamePrivate) {
-//         return omit(story, 'victimLastName');
-//       }
-//
-//       return story;
-//     });
-//   },
-//
-//   async findOne(params) {
-//     const story = await strapi
-//       .query('story')
-//       .findOne(params);
-//
-//     if (story.hasLastNamePrivate) {
-//       return omit(story, 'victimLastName');
-//     }
-//
-//     return story;
-//   },
-// };
+
+module.exports = {
+  async create(data, { files } = {}) {
+    const validData = await strapi.entityValidator.validateEntityCreation(
+      strapi.models.restaurant,
+      data,
+      { isDraft: isDraft(data, strapi.models.story) }
+    );
+
+    const entry = await strapi.query('story').create({...validData, published_at: null});
+
+    if (files) {
+      // automatically uploads the files based on the entry and the model
+      await strapi.entityService.uploadFiles(entry, files, {
+        model: 'story',
+        // if you are using a plugin's model you will have to add the `source` key (source: 'users-permissions')
+      });
+      return this.findOne({ id: entry.id });
+    }
+
+    return entry;
+  }
+};
